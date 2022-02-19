b9bce82d42b8d1f3ca884f007ac11263f358a2dd
adityakumarsinha03 <54764964+adityakumarsinha03@users.noreply.github.com>
2022-02-19 09:35:43 +0530

    Time: 4 ms (73.31%), Space: 8 MB (36.29%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.cpp
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -1,5 +1,39 @@
 class Solution {
 public:
+  /*  vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+        for(int i=1;i<=n;i++){
+            if(i%15==0)
+                ans.push_back("FizzBuzz");
+            else if(i%3==0)
+                ans.push_back("Fizz");
+            else if(i%5==0)
+                ans.push_back("Buzz");
+            else{
+                ans.push_back(to_string(i));
+            }
+        }
+        return ans;
+    }   */
+    
+    
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+        for(int i=1;i<=n;i++){
+            string s = "";
+            if(i%3==0)
+                s+="Fizz";
+            if(i%5==0)
+                s+="Buzz";
+            if(s.size()==0)
+                s+=to_string(i);
+            
+            ans.push_back(s);
+        }
+        return ans;
+    }
+    
+    /*
     vector<string> fizzBuzz(int n) {
         vector<string> ans;
         for(int i=1;i<=n;i++){
@@ -12,9 +46,7 @@ public:
             else{
                 ans.push_back(to_string(i));
             }
-                          
-            
         }
         return ans;
-    }
+    }*/
 };
