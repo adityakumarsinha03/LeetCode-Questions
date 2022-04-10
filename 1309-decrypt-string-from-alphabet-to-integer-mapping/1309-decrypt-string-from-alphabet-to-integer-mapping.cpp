a683ac8c2b880f94c78ec58c4854fb1a75a7ee5f
adityakumarsinha03 <54764964+adityakumarsinha03@users.noreply.github.com>
2022-04-10 18:36:47 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (93.59%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -4,12 +4,12 @@ public:
         string ans = "";
         for(int i=0; i<s.size(); i++){
             if(i+2<s.size() && s[i+2]=='#'){
-                string temp = s.substr(i,2);
-                int n = stoi(temp);
+               // string temp = s.substr(i,2);
+               // int n = stoi(temp);
+                int n = (s[i]-'0')*10 + (s[i+1]-'0');
                 ans+=(char(n+'a'-1));
                 i+=2;
             }else{
-                //ans+=(char(s[i]+'a'-1));
                  ans += char(s[i]-'0'-1+'a');
             }
         }
