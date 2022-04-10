45aa279c24e3e144017c30e246a3622b68207894
adityakumarsinha03 <54764964+adityakumarsinha03@users.noreply.github.com>
2022-04-10 18:54:36 +0530

    Time: 12 ms (18.35%), Space: 9.6 MB (53.81%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
@@ -1,33 +1,27 @@
 class Solution {
-public: 
-    unordered_map<int, int> um;
-    
-    bool checkOrder(string & a, string & b){
-        for(int i=0; i< min(a.size(), b.size()); i++){
-            if(um[a[i]] != um[b[i]]){
-                if(um[a[i]] < um[b[i]])
-                    return true;
-                else
+public:
+    unordered_map<int,int>um;
+    bool check(string a, string b){
+        for(int i=0;i<min(a.size(), b.size());i++){
+            if(a[i]!=b[i]){
+                if(um[b[i]]<um[a[i]]){
                     return false;
+                }else{
+                    return true;
+                }
             }
         }
-        
-        //return a.size()<b.size();
-        if(a.size()<=b.size())
-            return true;
-        else
-            return false;
+        return a.size()<=b.size();
     }
-    
-    
     bool isAlienSorted(vector<string>& words, string order) {
+        
         for(int i=0;i<order.size();i++){
-            um[order[i]] = i;
+            um[order[i]]=i;
         }
-        for(int i=0; i<words.size()-1; i++){
-            if(checkOrder(words[i], words[i+1]) == false)
+        for(int i=0;i<words.size()-1;i++){
+            if(!check(words[i],words[i+1]))
                 return false;
-        }  
+        }
         return true;
     }
 };
