839de9d94dd4a5d577e29fa09f9d75faf4145550
adityakumarsinha03 <54764964+adityakumarsinha03@users.noreply.github.com>
2022-04-12 15:22:17 +0530

    Time: 8 ms (80.34%), Space: 7.4 MB (45.98%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // if (s.size()!=t.size())
-        //     return false;
+        if(s.size()!=t.size())
+            return false;
         
+        unordered_map<char,int>um1, um2;
+        for(int i=0;i<s.size();i++){
+            um1[s[i]]++;
+            um2[t[i]]++;
+        }
+        return um1==um2;
+    }
+    /*
+    bool isAnagram(string s, string t) {
+              
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
         
         return s==t;            
-    }
+    }   */
 };
