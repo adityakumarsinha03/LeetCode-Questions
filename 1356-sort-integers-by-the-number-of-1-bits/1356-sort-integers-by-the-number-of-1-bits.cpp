8dff96a6ff758b508c2e3017f9e04db4cada7fc4
adityakumarsinha03 <54764964+adityakumarsinha03@users.noreply.github.com>
2022-04-12 14:58:39 +0530

    Time: 12 ms (62.91%), Space: 10.2 MB (70.57%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,6 +1,33 @@
 #include<bits/stdc++.h>
 class Solution {
 public:
+    
+    static int numberof1s(int a){
+        int count = 0;
+        while(a){
+            a = a&(a-1);
+            count++;
+        }
+        return count;
+    }
+    static bool compare(int a, int b){
+        int p = numberof1s(a);
+        int q = numberof1s(b);
+        if(p==q){
+            return (a<=b);
+        }
+        else{
+            return (p<q);
+        }
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(),arr.end(),compare);
+        return arr;
+    }
+    
+    
+    
+    /*  METHOD: 02
     static bool compare(pair<int,int>&a, pair<int,int>&b){
         if(a.second!=b.second)
             return a.second < b.second;
@@ -27,5 +54,5 @@ public:
             ans.push_back(v[i].first);
          }
         return ans;
-    }
+    } */
 };
