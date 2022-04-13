347c34f897fe7c09b49caecbb1caaaa2b9027c21
adityakumarsinha03 <54764964+adityakumarsinha03@users.noreply.github.com>
2022-04-13 23:00:23 +0530

    Time: 100 ms (70.28%), Space: 19.9 MB (86.71%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -7,16 +7,16 @@ public:
         }
         vector<vector<int>> res;
         for(int i=0;i<nums.size()-2;i++){
-          if(i==0 || nums[i]!=nums[i-1]){
+          if(i==0 || ( i>0 && nums[i]!=nums[i-1]) ){
             int low=i+1, high=nums.size()-1, sum=-nums[i]; 
             while(low<high){
                 if(nums[low]+nums[high]==sum){
-                   // res.push_back({nums[i], nums[low], nums[high]});
-                    vector<int> temp;
-                    temp.push_back(nums[i]);
-                    temp.push_back(nums[low]);
-                    temp.push_back(nums[high]);
-                    res.push_back(temp);
+                       res.push_back({nums[i], nums[low], nums[high]});
+                    // vector<int> temp;
+                    // temp.push_back(nums[i]);
+                    // temp.push_back(nums[low]);
+                    // temp.push_back(nums[high]);
+                    // res.push_back(temp);
                     while(low<high && nums[low]==nums[low+1]){
                         low++;
                     }while(low<high && nums[high]==nums[high-1]){
