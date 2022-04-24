fc17e38cf263a1a99bfb584422fb17270c642c3f
adityakumarsinha03 <54764964+adityakumarsinha03@users.noreply.github.com>
2022-04-24 22:20:37 +0530

    Time: 3 ms (36.58%), Space: 6.5 MB (44.90%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    long long nCr(long long n, long long r){
+   /* long long nCr(long long n, long long r){
        r= min(r, n-r);
        long long res=1;
        for(int i=0;i<r;i++){
@@ -9,10 +9,25 @@ public:
        }
         return res;
     }
-    
     int uniquePaths(int m, int n) {
-
         // return nCr(n+m-2, m-1);
          return nCr(n+m-2, n-1);
+    }  */
+    
+    
+    int helper(int i, int j, int m, int n, vector<vector<int>> &dp){
+        if(i>=m || j>=n)
+            return 0;
+        if(i==m-1 && j==n-1)
+            return 1;
+        
+        if(dp[i][j]!=-1) 
+            return dp[i][j];
+        
+        return dp[i][j]= helper(i+1, j, m, n, dp) + helper(i, j+1, m, n, dp);
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return helper(0, 0, m, n, dp);
     }
 };
