c364b54cf031e08b6eb6c55e2a872782d3e61cbf
adityakumarsinha03 <54764964+adityakumarsinha03@users.noreply.github.com>
2022-04-24 19:47:55 +0530

    Time: 39 ms (12.55%), Space: 19.6 MB (65.49%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
-    
-  /*1. Basic idea of the algorithm is if we cancel out each occurrence of an element e,
-with all the other elements that are different from e then e will exist till end if it is a majority element. 
-2. Below code loops through each element and maintains a count of the element that has the potential of being the majority element.
-3. If next element is same then increments the count, otherwise decrements the count.
-4. If the count reaches 0 then update the potential index to the current element and sets count to 1.*/
-    
     int majorityElement(vector<int>& nums) {
-       int ans=nums[0], count=1;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==ans){
-                count++;
-            }else{
-                count--;
-            }
+        int count=0;
+        int candidate;
+        for(auto num: nums){
+            if(count==0)
+                candidate=num;
             
-            if(count==0){
-                ans=nums[i];
-                count=1;
-            }
+            if(num== candidate)
+                count++;
+            else
+                count--;
         }
-        return ans;
-    } 
+        return candidate;
+    }
 };
