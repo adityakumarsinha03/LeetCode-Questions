1344e08d203cf33ef01d799301a3b99dde434868
adityakumarsinha03 <54764964+adityakumarsinha03@users.noreply.github.com>
2022-06-01 20:20:00 +0530

    Time: 36 ms (36.18%), Space: 10.9 MB (21.48%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<int> set;
+        int ans=0, l=0;
+        for(int r=0; r<s.size(); r++){     //travelling in outer loop 
+            if(set.find(s[r])!=set.end()){     //dublicate element found
+                while(l<r && set.find(s[r])!=set.end() ){
+                    set.erase(s[l]);
+                    l++;
+                }
+            }
+            set.insert(s[r]);
+            ans=max(ans, r-l+1);
+        }
+        return ans;
+    }
+    
+    /*  Sliding Window - Aditya Verma  ->  O(2n) || O(n)
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char,int>um;
+        int mx=0,i=0,j=0;
+        while(j<s.size()){
+            um[s[j]]++;
+            if(um.size()==j-i+1){
+                mx=max(mx,j-i+1);
+                j++;
+            }else if(um.size()<j-i+1){
+                while(um.size()<j-i+1){
+                    um[s[i]]--;
+                    if(um[s[i]]==0)
+                        um.erase(s[i]);
+                    i++;
+                }
+                j++;
+            }
+        }
+        return mx;
+    }  */
+};
