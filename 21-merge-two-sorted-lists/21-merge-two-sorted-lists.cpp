5468212b40ded8ec98d0349ccc0a3420ff252a89
adityakumarsinha03 <54764964+adityakumarsinha03@users.noreply.github.com>
2022-06-03 00:30:46 +0530

    Time: 20 ms (8.92%), Space: 14.8 MB (81.96%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -16,6 +16,46 @@ public:
         if(list2==NULL)
             return list1;
         
+       ListNode* fh= NULL, *ft= NULL;    //fh: final head
+       if(list1->val <= list2->val){
+           fh=list1;
+           ft=list1;
+           list1=list1->next;
+       }else{
+           fh=list2;
+           ft=list2;
+           list2=list2->next;
+       } 
+       
+        while(list1!=NULL && list2!=NULL){
+            if(list1->val < list2->val){
+                ft->next= list1;
+                ft=list1;
+                list1= list1->next;
+            }else{
+                ft->next= list2;
+                ft=list2;
+                list2= list2->next;
+            }
+        }
+        
+        if(list1!=NULL){
+            ft->next=list1;
+        }if(list2!=NULL){
+            ft->next=list2;
+        }
+        
+        return fh;
+    }    
+    
+    
+    /*   //STRIVER  
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL)
+            return list2;
+        if(list2==NULL)
+            return list1;
+        
         if(list1->val > list2->val){
             swap(list1, list2);
         }
@@ -30,5 +70,5 @@ public:
             swap(list1, list2);
         }
         return res;
-    }
+    }  */
 };
