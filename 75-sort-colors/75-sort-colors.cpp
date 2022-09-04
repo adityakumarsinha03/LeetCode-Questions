a7a94847338f4f818e74d3321fb1516dc2b51cc9
Aditya Kumar Sinha <54764964+adityakumarsinha03@users.noreply.github.com>
2022-09-04 11:59:10 +0530

    Time: 8 ms (11.71%), Space: 8.3 MB (70.38%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
+    //DUTCH NATIONAL FLAG ALGORITHM
     void sortColors(vector<int>& nums) {
-        int i=0, j=nums.size()-1, k=0;
-        while(k<=j){
-            if(nums[k]==0){
-                swap(nums[i], nums[k]);
-                k++, i++;
-            }else if(nums[k]==1){
-                k++;
+        int lo=0, mid=0, hi=nums.size()-1;
+        while(mid<=hi){
+            if(nums[mid]==0){
+                swap(nums[lo], nums[mid]);
+                lo++, mid++;
+            }else if(nums[mid]==1){
+                mid++;
             }else{
-                swap(nums[k], nums[j]);
-                j--;
+                swap(nums[mid], nums[hi]);
+                hi--;
             }
         }
     }
+    
+  /*  void sortColors(vector<int>& nums) {
+        int lo=0, mid=0, hi=nums.size()-1;
+        while(mid<=hi){
+            switch(nums[mid]){
+                case 0:
+                    swap(nums[lo++], nums[mid++]);
+                    break;
+                
+                case 1:
+                    mid++;
+                    break;
+                
+                case 2:
+                    swap(nums[mid], nums[hi--]);
+                    break;
+            }
+        }
+    }  */
 };
